7572d418f9e98808f467617e653da89405d085ed
Nbiba Bedis <bedisnbiba@gmail.com>
2024-04-15 20:51:57 +0100

    style

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,24 @@ const n1 = document.querySelector("#n1")! as HTMLInputElement;
 const n2 = document.querySelector("#n2")! as HTMLInputElement;
 const n3 = document.querySelector("#n3")! as HTMLInputElement;
 const n4 = document.querySelector("#n4")! as HTMLInputElement;
+const rel = document.querySelector("#rel")! as HTMLParagraphElement;
 
 n1.onchange = calc;
 n2.onchange = calc;
 n3.onchange = calc;
 
 function calc() {
-  if (!n1.value || !n2.value || !n3.value) {
+  if (!n1.value || !n2.value) {
+    rel.innerText = "";
     n4.value = "";
     return;
   }
-  n4.value = ((Number.parseFloat(n2.value) / Number.parseFloat(n1.value)) *
-    Number.parseFloat(n3.value)).toString();
+  const relVal = Number.parseFloat(n2.value) / Number.parseFloat(n1.value);
+  rel.innerText = "x " + relVal.toString();
+
+  if (!n3.value) {
+    n4.value = "";
+    return;
+  }
+  n4.value = (relVal * Number.parseFloat(n3.value)).toString();
 }
